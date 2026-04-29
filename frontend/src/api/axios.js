156ff40ba7ae0d8ae00fb796b57ff045ebcf34a5
Divyakunjumadan <230701082@rajalakshmi.edu.in>
2026-04-29 11:50:30 +0530

    fix api base path properly

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,29 +1,31 @@
-import axios from 'axios'
-
-const API_URL = "https://service-finder-backend-divi-dve4f2hpb7bfceek.centralindia-01.azurewebsites.net/api"
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: API_URL,
-  headers: { 'Content-Type': 'application/json' }
-})
+  baseURL: "https://service-finder-backend-divi-dve4f2hpb7bfceek.centralindia-01.azurewebsites.net",
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
 
 // Attach token
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('sf_token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
-  return config
-})
+  const token = localStorage.getItem('sf_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 // Handle 401
 api.interceptors.response.use(
   (res) => res,
   (err) => {
     if (err.response?.status === 401) {
-      localStorage.removeItem('sf_token')
-      window.location.href = '/login'
+      localStorage.removeItem('sf_token');
+      window.location.href = '/login';
     }
-    return Promise.reject(err)
+    return Promise.reject(err);
   }
-)
+);
 
-export default api
+export default api;
