864973e20db818416e89272b269d13d4da4aef0e
Divyakunjumadan <230701082@rajalakshmi.edu.in>
2026-04-29 11:44:59 +0530

    fix cors properly

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,10 +19,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({
-  origin: "https://service-finder-omega.vercel.app",
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
